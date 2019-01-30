8fe1e681e32b6322bd30088a5c16f50cf4a1e3fc
disrael <disrael@uclusion.com>
2019-01-30 14:35:15 -0800

    delete category

--- tests/listsTest.js
+++ tests/listsTest.js
@@ -86,6 +86,8 @@ module.exports = function(adminConfiguration, userConfiguration, userId) {
             }).then((response) => {
                 return globalClient.investibles.delete(globalCSMInvestibleId);
             }).then((response) => {
+                return globalClient.investibles.deleteCategory('sandwich', globalMarketId);
+            }).then((response) => {
                 return globalClient.markets.deleteMarket(globalMarketId);
             }).catch(function(error) {
                 console.log(error);
