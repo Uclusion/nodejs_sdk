214ba0b18668b2af316dbdc522232d92c9f60a44
disrael <disrael@uclusion.com>
2019-04-19 16:11:41 -0700

    fix: provision dev market

--- provision/market.js
+++ provision/market.js
@@ -4,29 +4,29 @@ import {uclusion} from "../src/uclusion";
 global.fetch = fetch;
 
 const adminConfiguration = {
-    baseURL:  'https://stage.api.uclusion.com/v1',
-    websocketURL: 'wss://stage.ws.uclusion.com/v1'
+    baseURL:  'https://dev.api.uclusion.com/v1',
+    websocketURL: 'wss://dev.ws.uclusion.com/v1'
 };
 
 const userConfiguration = {
-    baseURL:  'https://stage.api.uclusion.com/v1',
-    websocketURL: 'wss://stage.ws.uclusion.com/v1'
+    baseURL:  'https://dev.api.uclusion.com/v1',
+    websocketURL: 'wss://dev.ws.uclusion.com/v1'
 };
 
 const adminAuthorizerConfiguration = {
     username: 'testeruclusion@gmail.com',
     password: 'Uclusi0n_test',
-    poolId: 'us-west-2_xNQkCChUO',
-    clientId: '6umnmeui65283qk6mgciljktrl',
-    baseURL:  'https://stage.api.uclusion.com/v1',
+    poolId: 'us-west-2_NVPcNPhKS',
+    clientId: '4knr08iqujrprrkpimqm04dnp',
+    baseURL:  'https://dev.api.uclusion.com/v1',
 };
 
 const userAuthorizerConfiguration = {
     username: '827hooshang@gmail.com',
     password: 'Uclusi0n_test',
-    poolId: 'us-west-2_xNQkCChUO',
-    clientId: '6umnmeui65283qk6mgciljktrl',
-    baseURL:  'https://stage.api.uclusion.com/v1',
+    poolId: 'us-west-2_NVPcNPhKS',
+    clientId: '4knr08iqujrprrkpimqm04dnp',
+    baseURL:  'https://dev.api.uclusion.com/v1',
 };
 
 function sleep(ms) {
