560c87559230eb9caa6d8e57e0a90bdf6cde931d
Benjamin Follis <bfollis@uclusion.com>
2019-08-02 19:03:35 -0700

    Added new form of test logins

--- test/uclusionTest.js
+++ test/uclusionTest.js
@@ -30,10 +30,10 @@ const userConfiguration = {
 describe('uclusion', () => {
     require('../tests/usersTest.js')(adminConfiguration, userConfiguration);
     require('../tests/ssoTest.js')(adminConfiguration, userConfiguration);
-//    require('../tests/marketsTest.js')(adminConfiguration);
-//    require('../tests/marketInvestiblesTest')(adminConfiguration);
-//    require('../tests/investmentsTest.js')(adminConfiguration, userConfiguration, 2);
-//    require('../tests/listsTest.js')(adminConfiguration, userConfiguration);
+    require('../tests/marketsTest.js')(adminConfiguration);
+    require('../tests/marketInvestiblesTest')(adminConfiguration);
+    require('../tests/investmentsTest.js')(adminConfiguration, userConfiguration, 2);
+    require('../tests/listsTest.js')(adminConfiguration, userConfiguration);
 });
 
 
--- tests/investmentsTest.js
+++ tests/investmentsTest.js
@@ -3,8 +3,9 @@ import uclusion from 'uclusion_sdk';
 import { WebSocketRunner } from '../src/WebSocketRunner';
 import { arrayEquals, sleep } from './commonTestFunctions';
 import {CognitoAuthorizer} from 'uclusion_authorizer_sdk';
+import {loginUserToAccount, loginUserToMarket} from "../src/utils";
 
-module.exports = function (adminConfiguration, userConfiguration, adminAuthorizerConfiguration, userAuthorizerConfiguration, numUsers) {
+module.exports = function (adminConfiguration, userConfiguration, numUsers) {
     const fishOptions = {
         name: 'fish',
         description: 'this is a fish market',
@@ -20,79 +21,71 @@ module.exports = function (adminConfiguration, userConfiguration, adminAuthorize
 
     describe('#doInvestment', () => {
         it('should create investment without error', async () => {
-            let promise = uclusion.constructClient(adminConfiguration);
-            let globalClient;
-            let globalUserClient;
-            let globalUserId;
-            let globalMarketId;
+            let promise = loginUserToAccount(adminConfiguration, adminConfiguration.accountId);
+            let adminClient;
+            let userClient;
+            let userId;
+            let createdMarketId;
             let marketInvestibleId;
             let globalStages;
             await promise.then((client) => {
-                globalClient = client;
-                return globalClient.markets.createMarket(fishOptions);
+                adminClient = client;
+                return adminClient.markets.createMarket(fishOptions);
             }).then((response) => {
-                const configuration = {...adminConfiguration};
-                const adminAuthorizerConfig = {...adminAuthorizerConfiguration};
-                adminAuthorizerConfig.marketId = response.market_id;
-                configuration.authorizer = new CognitoAuthorizer(adminAuthorizerConfig);
-                globalMarketId = response.market_id;
-                return uclusion.constructClient(configuration);
+                createdMarketId = response.market_id;
+                return loginUserToMarket(adminConfiguration, createdMarketId);
             }).then((client) => {
-                globalClient = client;
-                console.log('Market ID is ' + globalMarketId);
-                const userConfig = {...userConfiguration};
-                const userAuthorizerConfig = {...userAuthorizerConfiguration};
-                userAuthorizerConfig.marketId = globalMarketId;
-                userConfig.authorizer = new CognitoAuthorizer(userAuthorizerConfig);
-                return uclusion.constructClient(userConfig);
+                adminClient = client;
+                console.log('Market ID is ' + createdMarketId);
+                return loginUserToMarket(userConfiguration, createdMarketId);
             }).then((client) => {
-                globalUserClient = client;
+                userClient = client;
                 return sleep(7000);
-            }).then(() => globalUserClient.users.get()).then((user) => {
+            }).then(() => userClient.users.get()).then((user) => {
                 let userPresence = user.market_presence;
                 assert(userPresence.quantity === fishOptions.new_user_grant, 'Quantity is ' + userPresence.quantity);
                 webSocketRunner.connect();
-                webSocketRunner.subscribe(user.id, { market_id : globalMarketId });
-                globalUserId = user.id;
-                return globalUserClient.investibles.create('salmon', 'good on bagels');
+                webSocketRunner.subscribe(user.id, { market_id : createdMarketId });
+                userId = user.id;
+                return userClient.investibles.create('salmon', 'good on bagels');
             }).then((response) => {
                 marketInvestibleId = response.id;
                 console.log('Investible ID is ' + marketInvestibleId);
-                return globalClient.users.grant(globalUserId, 9000);
+                return adminClient.users.grant(userId, 9000);
             }).then((response) => {
                 // Give async processing time to complete - including the grants to user
                 return sleep(5000);
             }).then((response) => {
-                return globalUserClient.markets.updateInvestment(marketInvestibleId, 2000, 0);
+                return userClient.markets.updateInvestment(marketInvestibleId, 2000, 0);
             }).then((investment) => {
                 assert(investment.quantity === 2000, 'investment quantity should be 2000');
-                return globalUserClient.investibles.follow(marketInvestibleId, false);
+                return userClient.investibles.follow(marketInvestibleId, false);
             }).then((response) => {
                 assert(response.following === true, 'follow should return true');
-                return globalUserClient.investibles.createComment(marketInvestibleId, 'body of my comment');
+                return userClient.investibles.createComment(marketInvestibleId, 'body of my comment');
             }).then((response) => {
                 return webSocketRunner.waitForReceivedMessage({event_type: 'INVESTIBLE_COMMENT_UPDATED'})
                   .then((payload) => response);
             }).then((comment) => {
                 assert(comment.body === 'body of my comment', 'comment body incorrect');
                 assert(comment.is_official === false, 'comment should not be official');
-                return globalUserClient.investibles.updateComment(comment.id, 'new body');
+                return userClient.investibles.updateComment(comment.id, 'new body');
             }).then((comment) => {
                 assert(comment.body === 'new body', 'updated comment body incorrect');
-                return globalClient.investibles.createComment(marketInvestibleId, 'comment to fetch');
+                return adminClient.investibles.createComment(marketInvestibleId, 'comment to fetch');
             }).then((comment) => {
                 assert(comment.body === 'comment to fetch', 'comment body incorrect');
                 assert(comment.is_official === true, 'comment should be official');
-                return globalUserClient.investibles.getMarketComments([comment.id]);
+                return userClient.investibles.getMarketComments([comment.id]);
             }).then((comments) => {
                 let comment = comments[0];
                 assert(comment.body === 'comment to fetch', 'fetched comment body incorrect');
-                assert(comment.market_id === globalMarketId, 'market was not set properly on the comment');
-                return globalUserClient.users.get();
+                assert(comment.market_id === createdMarketId, 'market was not set properly on the comment');
+                return userClient.users.get();
             }).then((user) => {
                 let userPresence = user.market_presence;
                 assert(userPresence.quantity === fishOptions.new_user_grant + 7000, 'Quantity was instead ' + userPresence.quantity + ' for ' + user.id);
-                return globalClient.investibles.update(marketInvestibleId, updateFish.name,
+                return adminClient.investibles.update(marketInvestibleId, updateFish.name,
                     updateFish.description, updateFish.label_list);
             }).then((response) => {
                 return webSocketRunner.waitForReceivedMessage({event_type: 'MARKET_INVESTIBLE_UPDATED', object_id: marketInvestibleId})
@@ -101,7 +94,7 @@ module.exports = function (adminConfiguration, userConfiguration, adminAuthorize
                 assert(response.name === 'pufferfish', 'update market investible name not passed on correctly');
                 assert(response.description === 'possibly poisonous', 'update market investible description not passed on correctly');
                 assert(arrayEquals(response.label_list, ['freshwater', 'spawning']), 'update market investible label list not passed on correctly');
-                return globalUserClient.markets.getMarketInvestibles([marketInvestibleId]);
+                return userClient.markets.getMarketInvestibles([marketInvestibleId]);
             }).then((investibles) => {
                 let investible = investibles[0];
                 assert(investible.name === 'pufferfish', 'get market investible name incorrect');
@@ -109,10 +102,10 @@ module.exports = function (adminConfiguration, userConfiguration, adminAuthorize
                 assert(arrayEquals(investible.label_list, ['freshwater', 'spawning']), 'update market investible label list not passed on correctly');
                 assert(investible.quantity === 2000, 'get market investible quantity incorrect');
                 assert(investible.current_user_is_following === true, 'current_user_is_following should return true');
-                return globalClient.markets.listStages();
+                return adminClient.markets.listStages();
             }).then((stages) => {
                 globalStages = stages;
-                return globalUserClient.markets.get();
+                return userClient.markets.get();
             }).then((market) => {
                 //console.log(market);
                 assert(market.active_investments === 2000, 'active investments should be 2000');
@@ -124,24 +117,24 @@ module.exports = function (adminConfiguration, userConfiguration, adminAuthorize
                     current_stage_id: current_stage.id,
                     stage_id: stage.id
                 };
-                return globalClient.investibles.stateChange(marketInvestibleId, stateOptions);
+                return adminClient.investibles.stateChange(marketInvestibleId, stateOptions);
             }).then((response) => {
                 return sleep(10000);
             }).then((response) => {
-                return globalClient.summaries.marketSummary();
+                return adminClient.summaries.marketSummary();
             }).then((summaries) => {
-                assert(summaries.market_id === globalMarketId);
+                assert(summaries.market_id === createdMarketId);
                 assert(summaries.summaries.length === 1, 'There should be 1 day of summary data for a new market');
                 const todaysSummary = summaries.summaries[0];
                 assert(todaysSummary.unspent_shares === 2*fishOptions.new_user_grant + 7000, 'Unspent wrong in summary');
                 assert(todaysSummary.num_users === numUsers, 'There are ' + todaysSummary.num_users + ' in the market');
-                return globalUserClient.markets.updateInvestment(marketInvestibleId, 0, 2000);
+                return userClient.markets.updateInvestment(marketInvestibleId, 0, 2000);
             }).then((response) => {
                 return webSocketRunner.waitForReceivedMessage({event_type: 'MARKET_INVESTIBLE_UPDATED', object_id: marketInvestibleId})
                     .then((payload) => response);
             }).then(() => {
                 return webSocketRunner.terminate();
-            }).then(() => globalUserClient.markets.getMarketInvestibles([marketInvestibleId])
+            }).then(() => userClient.markets.getMarketInvestibles([marketInvestibleId])
             ).then((investibles) => {
                 let investible = investibles[0];
                 const current_stage = globalStages.find(stage => { return stage.name === 'Needs Investment'});
@@ -150,7 +143,7 @@ module.exports = function (adminConfiguration, userConfiguration, adminAuthorize
                 assert(investible.open_for_refunds === true, 'open_for_refunds true');
                 assert(investible.is_active === true, 'is_active true');
                 assert(investible.quantity === 0, 'investment should be updated to zero');
-                return globalClient.markets.deleteMarket();
+                return adminClient.markets.deleteMarket();
             }).catch(function (error) {
                     console.log(error);
                     //close our websocket
