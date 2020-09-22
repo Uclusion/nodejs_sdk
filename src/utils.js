17080e0072887d55dd03cbc8ad94cbb813f75ae0
disrael <disrael@gmail.com>
2020-09-22 13:37:08 -0700

    fix: start switching over to using account token as support is dropping for Cognito one

--- src/utils.js
+++ src/utils.js
@@ -57,7 +57,10 @@ export function loginUserToAccountAndGetToken(configuration) {
             const tokenManager = new TestTokenManager(TOKEN_TYPE_ACCOUNT, null, ssoClient);
             return tokenManager.getToken()
                 .then((accountToken) => {
-                    return {accountToken, client: uclusion.constructClient({ ...configuration, tokenManager })};
+                    return uclusion.constructClient({ ...configuration, tokenManager })
+                        .then((client) => {
+                            return {accountToken, client};
+                        })
                 });
         });
 }
