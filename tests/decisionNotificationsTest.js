067f65addfba7c91d9662042a173788bad34c96b
disrael <disrael@gmail.com>
2022-08-29 08:23:46 -0700

    fix: be more careful with groups

--- tests/decisionNotificationsTest.js
+++ tests/decisionNotificationsTest.js
@@ -33,7 +33,8 @@ module.exports = function (adminConfiguration, userConfiguration) {
                 console.log(`Logging admin into market ${createdMarketId}`);
                 return loginUserToMarketInvite(adminConfiguration, createdMarketInvite);
             }).then((client) => {
-                return client.investibles.createComment(undefined, createdMarketId, 'Is it done?', null, 'QUESTION');
+                return client.investibles.createComment(undefined, createdMarketId, 'Is it done?',
+                    null, 'QUESTION');
             }).then((comment) => {
                 globalCommentId = comment.id;
                 return adminConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'comment',
@@ -62,8 +63,6 @@ module.exports = function (adminConfiguration, userConfiguration) {
                 return loginUserToMarketInvite(userConfiguration, createdMarketInvite);
             }).then((client) => {
                 userClient = client;
-                return userConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'notification'});
-            }).then(() => {
                 return getMessages(userConfiguration);
             }).then((messages) => {
                 const vote = messages.find(obj => {
@@ -74,11 +73,13 @@ module.exports = function (adminConfiguration, userConfiguration) {
             }).then((user) => {
                 userId = user.id;
                 userExternalId = user.external_id;
-                return userClient.investibles.create({groupId: createdMarketId, name: 'salmon', description: 'good on bagels'});
+                return userClient.investibles.create({groupId: createdMarketId, name: 'salmon',
+                    description: 'good on bagels'});
             }).then((investible) => {
                 marketInvestibleId = investible.investible.id;
                 console.log('Investible ID is ' + marketInvestibleId);
-                return adminConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'notification', object_id: adminExternalId});
+                return adminConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'notification',
+                    object_id: adminExternalId});
             }).then(() => {
                 return getMessages(userConfiguration);
             }).then((messages) => {
@@ -92,10 +93,12 @@ module.exports = function (adminConfiguration, userConfiguration) {
                     return obj.type_object_id === 'INVESTIBLE_SUBMITTED_' + marketInvestibleId;
                 });
                 assert(submitted, 'Should receive investible submitted for new investible');
-                return adminClient.investibles.createComment(marketInvestibleId, createdMarketId, 'body of my comment', null, 'QUESTION');
+                return adminClient.investibles.createComment(marketInvestibleId, createdMarketId,
+                    'body of my comment', null, 'QUESTION');
             }).then((comment) => {
                 createdCommentId = comment.id;
-                return userConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'notification', object_id: userExternalId});
+                return userConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'notification',
+                    object_id: userExternalId});
             }).then(() => {
                 return getMessages(userConfiguration);
             }).then((messages) => {
@@ -111,7 +114,8 @@ module.exports = function (adminConfiguration, userConfiguration) {
                 assert(!submitted, 'Investible submitted removed if leave comment');
                 return userClient.investibles.updateComment(createdCommentId, undefined, true);
             }).then(() => {
-                return adminConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'comment', object_id: createdMarketId});
+                return adminConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'comment',
+                    object_id: createdMarketId});
             }).then(() => {
                 return getMessages(userConfiguration);
             }).then((messages) => {
@@ -151,7 +155,8 @@ module.exports = function (adminConfiguration, userConfiguration) {
                     return obj.type_object_id === 'UNREAD_OPTION_' + marketInvestibleId;
                 });
                 assert(!newOption, 'View notification not required since will have not fully voted');
-                return userClient.markets.updateInvestment(marketInvestibleId, 100, 0);
+                return userClient.markets.updateInvestment(marketInvestibleId, 100,
+                    0);
             }).then(() => {
                 return adminConfiguration.webSocketRunner.waitForReceivedMessage({event_type: 'notification',
                     object_id: adminExternalId});
@@ -162,7 +167,8 @@ module.exports = function (adminConfiguration, userConfiguration) {
                     return obj.type_object_id === 'NOT_FULLY_VOTED_' + createdMarketId;
                 });
                 assert(!vote, 'Not fully voted removed on approving an option');
-                return userClient.investibles.createComment(marketInvestibleId, createdMarketId, 'body of my comment', null,
+                return userClient.investibles.createComment(marketInvestibleId, createdMarketId,
+                    'body of my comment', null,
                     'ISSUE');
             }).then((comment) => {
                 createdCommentId = comment.id;
@@ -175,7 +181,8 @@ module.exports = function (adminConfiguration, userConfiguration) {
                     return obj.type_object_id === 'NOT_FULLY_VOTED_' + createdMarketId;
                 });
                 assert(vote, 'Not fully voted restored on option demoted by issue');
-                return userClient.investibles.createComment(marketInvestibleId, createdMarketId, 'body of my comment', null,
+                return userClient.investibles.createComment(marketInvestibleId, createdMarketId,
+                    'body of my comment', null,
                     'ISSUE');
             }).catch(function (error) {
                 console.log(error);
